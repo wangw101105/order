3393058b21095e19b817491177a5704d2e8b938f
wangww <wangww@servyou.com.cn>
2019-05-31 15:55:11 +0800

    deploy.sh配置提交

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
-# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+export PROJ_PATH=/home/workspace
 
 # 输入你的环境上tomcat的全路径
-# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+export TOMCAT_APP_PATH=/usr/local/tomcat8-test
 
 ### base 函数
 killTomcat()
